49b37a989d5719a9890c849cff89ebb095f2b957
hamsa-joshua <joshuahamsaa@gmail.com>
2024-11-09 00:09:49 +0530

    Basic Queries

---
+++ Bank.session.sql
@@ -0,0 +1,19 @@
+SHOW DATABASES;
+
+DROP DATABASE Bank;
+
+SHOW TABLES;
+
+USE Bank;
+
+SELECT * FROM Customers;
+
+
+SELECT account_number FROM Customers;
+
+
+SELECT * FROM josh_transaction;
+
+DROP TABLE Customers;
+
+DROP TABLE mahi_transaction;
